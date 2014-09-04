20465654b88696849b6d1d545317f36a902a2007
Ales Guzik <me@aguzik.net>
2014-09-05 01:45:32 +0300

    orientdb persistence

--- src/workspace/persistence/orient.clj
+++ src/workspace/persistence/orient.clj
@@ -1,3 +1,15 @@
 (ns workspace.persistence.orient
   (:require
-   [clj-orient.core :as o]))
+   [clj-orient
+    [graph :as g]
+    [core :as o]]))
+;; (o/create-db! "memory:workspace")
+;; (o/create-db! "plocal:workspace")
+;; (o/create-db! "remote:workspace")
+
+;; (def db
+;;   (g/open-graph-db! "memory:workspace" "admin" "admin"))
+
+;; (def db2
+;;   (g/open-graph-db! "local:localhost/workspace" "admin" "admin"))
+(g/open-graph-db! "remote:localhost/workspace" "admin" "admin")
